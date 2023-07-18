7ec663b8fc16a05e464cdc504ace739da441ef72
David Vizena <davidvizena@gmail.com>
2023-07-18 16:25:42 -0500

    userController functionality built for logout/reg

--- .history/backend/controllers/userController_20230718162123.js
+++ .history/backend/controllers/userController_20230718162123.js
@@ -59,12 +59,7 @@ const registerUser = asyncHandler(async (req, res) => {
 // route   POST /api/users/logout
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
-	res.cookie('jwt', '', {
-		httpOnly: true,
-		expires: new Date(0),
-	});
-
-	res.status(200).json({ message: 'User logged out' });
+	res.status(200).json({ message: 'Logout user' });
 });
 
 // @desc   Get user profile

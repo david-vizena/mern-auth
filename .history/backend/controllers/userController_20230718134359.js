8b2be6bf976886e7f3178adfd22cbdf7bc066444
David Vizena <davidvizena@gmail.com>
2023-07-18 13:56:51 -0500

    errorHandler middleware created

--- .history/backend/controllers/userController_20230718134359.js
+++ .history/backend/controllers/userController_20230718134359.js
@@ -1,10 +1,8 @@
-import asyncHandler from 'express-async-handler';
-
 // @desc   Auth user/set token
 // route   POST /api/users/auth
 // @access Public
-const authUser = asyncHandler(async (req, res) => {
+const authUser = async (req, res) => {
 	res.status(200).json({ message: 'Auth user' });
-});
+};
 
 export { authUser };

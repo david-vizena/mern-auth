6ab302972d3b3c0e7310a85c320393a5881809a1
David Vizena <davidvizena@gmail.com>
2023-07-18 16:34:53 -0500

    authMiddleware created and pulled into userRoutes

---
+++ .history/backend/middleware/authMiddleware_20230718162832.js
@@ -0,0 +1,9 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+
+const protect = asyncHandler(async (req, res, next) => {
+	let token;
+
+	token = req.cookies.jwt;
+});
--- .history/backend/routes/userRoutes_20230718163337.js
+++ .history/backend/routes/userRoutes_20230718163337.js
@@ -12,9 +12,6 @@ import { protect } from '../middleware/authMiddleware.js';
 router.post('/', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
-router
-	.route('/profile')
-	.get(protect, getUserProfile)
-	.put(protect, updateUserProfile);
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
 
 export default router;
